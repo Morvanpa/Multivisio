68441f560854a38cbb81f3f3523587fceada1c3f
Paul Morvan <paul.morvan@ensta-bretagne.org>
2024-12-10 14:33:41 +0100

    replacement of the venv > requirements.txt

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-#Multivisio
+# Multivisio
+
+## Installation :
+
+Clone the git repository :
+
+```bash
+git clone https://github.com/Morvanpa/Multivisio.git
+```
+
+in the Multivisio folder, create a virtual environment : 
+
+```bash
+python3 -m venv venv
+```
+
+Install the packages specified in *requirements.txt* : 
+
+```bash
+pip install -r requirements.txt
+```
---
+++ lienPersonSuitcase.py
@@ -0,0 +1,73 @@
+import numpy as np
+from ultralytics import YOLO
+import cv2
+
+
+url = 'http://172.20.10.12:81/stream'
+video = 'ValiseTest.mp4'
+
+model = YOLO('weights/best.pt')
+
+
+def lienValisePersVideo(videoOrStream, model):
+    cameraIP=cv2.VideoCapture(videoOrStream)
+
+    if not cameraIP.isOpened():
+        print("Erreur 1")
+
+    cv2.namedWindow("output", cv2.WINDOW_NORMAL)  # Create window with freedom of dimensions
+    cv2.resizeWindow("output", int(1920/4), int(1080/4))
+
+    while True:
+        ret, frame = cameraIP.read()
+        if not ret:
+            print("Fin de la vidéo")
+            break
+
+        suitcase = []
+        person = []
+        result = model(frame)
+        for detection in result[0].boxes:
+            xmin, ymin, xmax, ymax = detection.xyxy[0].cpu().numpy()
+            confidence = detection.conf.cpu().numpy()
+            label = detection.cls.cpu().numpy()  # Object class
+
+            class_name = model.names[int(label)]
+            if class_name == "person":
+                person.append((int(xmin), int(ymin), int(xmax), int(ymax)))
+                #cv2.rectangle(frame, (int(xmin), int(ymin)), (int(xmax), int(ymax)), (255, 0, 0), 2)  # Blue for person
+
+            if class_name == "suitcase":
+                suitcase.append((int(xmin), int(ymin), int(xmax), int(ymax)))
+                #cv2.rectangle(frame, (int(xmin), int(ymin)), (int(xmax), int(ymax)), (0, 255, 0), 2)  # Green for suitcase
+
+        # Proximity check between persons and suitcases
+        for p in person:
+            deltaV = 80
+            deltaH = 100
+
+            for s in suitcase:
+                # Refine proximity check based on suitcase and person dimensions
+                if ((s[3] <= p[3] + deltaV) and (s[3] >= p[3] - deltaV)) and ((s[0] <= p[2] + deltaH) and (s[2] >= p[0] - deltaH)):
+                    
+                    # Draw a line connecting the center of the suitcase and the person
+                    cv2.line(frame, (s[0] + int(0.5 * (s[2] - s[0])), s[1] + int(0.5 * (s[3] - s[1]))),
+                             (p[0] + int(0.5 * (p[2] - p[0])), p[1] + int(0.5 * (p[3] - p[1]))), (255, 0, 0), 5)
+                else:
+                    
+                    cv2.imwrite("image.png", frame[p[1]:p[3], p[0]:p[2]] );
+
+        # Show the frame with annotations
+        cv2.imshow("output", frame)
+
+        # Exit on 'q' key press
+        if cv2.waitKey(10) & 0xff == ord('q'):
+            break
+
+    # Clean up resources
+    cameraIP.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    lienValisePersVideo(video, model)
